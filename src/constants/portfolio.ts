99868b7621cb6c2f977c1f62428a18f213ac385c
unknown <111201912289@mhs.dinus.ac.id>
2024-03-01 20:21:42 +0700

    update portfolio

--- src/constants/portfolio.ts
+++ src/constants/portfolio.ts
@@ -2,6 +2,102 @@ import type { Portfolio } from "@/types";
 
 export const portfolioList: Portfolio[] = [
   {
+    title: "Markashima Wedding Studio",
+    imageThumbnail: "/assets/images/project-markashima.png",
+    description:
+      "Project Web Booking System for Markashima Wedding Studio. made usin Next.js + Typescript, MySql, React and styled with Shadcn and Tailwind.",
+    livePreviewURL: "https://markashima.vercel.app",
+    repositoryURL: "https://github.com/Markashima/markashima",
+    techStack: [
+      {
+        tech: "Nextjs",
+        logo: "/assets/svg/next js.svg",
+      },
+      {
+        tech: "Typescript",
+        logo: "/assets/svg/typescript.svg",
+      },
+      {
+        tech: "React",
+        logo: "/assets/svg/react.svg",
+      },
+      {
+        tech: "tailwind",
+        logo: "/assets/svg/tailwind.svg",
+      },
+      {
+        tech: "Mysql",
+        logo: "/assets/svg/mysql.svg",
+      }
+    ],
+  },
+  {
+    title: "Improve Website MuTouch",
+    imageThumbnail: "/assets/images/project-mutouch.png",
+    description:
+      "Project to improving perfomance websites mu-touch.com by fixing SCP, LCP and connecting to DNS Cloudflare using wordpress. Also setting the stagging for development as well.",
+    livePreviewURL: "https://www.mu-touch.com/",
+    repositoryURL: "https://www.mu-touch.com/",
+    techStack: [
+      {
+        tech: "wordpress",
+        logo: "/assets/svg/wordpress.svg",
+      },
+      {
+        tech: "Mysql",
+        logo: "/assets/svg/mysql.svg",
+      }
+    ],
+  },
+  {
+    title: "ProudIT Company Profile",
+    imageThumbnail: "/assets/images/project-proudit.png",
+    description:
+      "Project Web Company Profile when interning at ProudIT. made usin laravel + Inertia.js (SSR) and React.",
+    livePreviewURL: "https://proud-it.com/",
+    repositoryURL: "https://github.com/Proud-IT/proudit-compro",
+    techStack: [
+      {
+        tech: "Laravel",
+        logo: "/assets/svg/laravel.svg",
+      },
+      {
+        tech: "React",
+        logo: "/assets/svg/react.svg",
+      },
+      {
+        tech: "tailwind",
+        logo: "/assets/svg/tailwind.svg",
+      },
+      {
+        tech: "Mysql",
+        logo: "/assets/svg/mysql.svg",
+      }
+    ],
+  },
+  {
+    title: "Landing Page MUA",
+    imageThumbnail: "/assets/images/project-kikira.png",
+    description:
+      "Personal Project Web Landing Page Make Up Artis for makeupbykikiraa. made usin laravel 10 and integrated with instagram basic display API.",
+    livePreviewURL: "https://makeupbykikiraa.vercel.app/",
+    repositoryURL: "https://github.com/vigorjs/Kikiras-MUA",
+    techStack: [
+      {
+        tech: "Laravel",
+        logo: "/assets/svg/laravel.svg",
+      },
+      {
+        tech: "PHP",
+        logo: "/assets/svg/php.svg",
+      },
+      {
+        tech: "Mysql",
+        logo: "/assets/svg/mysql.svg",
+      }
+    ],
+  },
+  {
     title: "Pegadaian Sistem Inventaris",
     imageThumbnail: "/assets/images/project-pesiris.png",
     description:
@@ -24,6 +120,28 @@ export const portfolioList: Portfolio[] = [
     ],
   },
   {
+    title: "Vehicle Booking System",
+    imageThumbnail: "/assets/images/project-vehicle-booking-system.png",
+    description:
+      "Personal Project App Pemesanan Kendaraan menggunakan Laravel + Stisla",
+    livePreviewURL: "https://github.com/vigorjs/vehicle-booking-system",
+    repositoryURL: "https://github.com/vigorjs/vehicle-booking-system",
+    techStack: [
+      {
+        tech: "Laravel",
+        logo: "/assets/svg/laravel.svg",
+      },
+      {
+        tech: "Stisla",
+        logo: "/assets/svg/stisla.svg",
+      },
+      {
+        tech: "Mysql",
+        logo: "/assets/svg/mysql.svg",
+      }
+    ],
+  },
+  {
     title: "eFootball League",
     imageThumbnail: "/assets/images/project-efootball.png",
     description:
@@ -43,28 +161,6 @@ export const portfolioList: Portfolio[] = [
         tech: "Mysql",
         logo: "/assets/svg/mysql.svg",
       },
-    ],
-  },
-  {
-    title: "Vehicle Booking System",
-    imageThumbnail: "/assets/images/project-vehicle-booking-system.png",
-    description:
-      "Personal Project App Pemesanan Kendaraan menggunakan Laravel + Stisla",
-    livePreviewURL: "https://github.com/inifarhan/rest-api-with-jwt-and-pagination",
-    repositoryURL: "https://github.com/inifarhan/rest-api-with-jwt-and-pagination",
-    techStack: [
-      {
-        tech: "Laravel",
-        logo: "/assets/svg/laravel.svg",
-      },
-      {
-        tech: "Stisla",
-        logo: "/assets/svg/stisla.svg",
-      },
-      {
-        tech: "Mysql",
-        logo: "/assets/svg/mysql.svg",
-      }
     ],
   },
 ];
